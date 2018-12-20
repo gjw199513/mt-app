60201ac0f56d670e6da8b6a0b145a63689cbed99
gjw199513 <gjw199513@163.com>
2018-12-20 15:22:18 +0800

    开发美团网首页+登录注册+切换城市页

---
+++ pages/changeCity.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="page-changeCity">
+    <el-row>
+      <el-col :span="24">
+        <iSelect/>
+      </el-col>
+    </el-row>
+    <el-row>
+      <el-col :span="24">
+        <hot/>
+      </el-col>
+    </el-row>
+    <el-row>
+      <el-col :span="24">
+        <category/>
+      </el-col>
+    </el-row>
+  </div>
+</template>
+
+<script>
+import iSelect from '../components/changeCity/iselect'
+import Hot from '../components/changeCity/hot'
+import Category from '../components/changeCity/category'
+
+export default {
+  name: 'ChangeCity',
+  components: { Category, Hot, iSelect }
+}
+</script>
+
+<style scoped>
+
+</style>
